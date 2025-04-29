94463b25f734b3af0580648e4f0bcab5ab521876
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-04-29 11:12:54 +0200

    Made blog class

---
+++ Lib/Model/Blog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Model
+{
+    internal class Blog
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public Member Author { get; set; }
+
+        public Blog(string title, string text, DateTime date, Member member)
+        {
+            Title = title;
+            Text = text;
+            Date = date;
+            Author = member;
+        }
+
+            
+
+
+
+
+    }
+}
