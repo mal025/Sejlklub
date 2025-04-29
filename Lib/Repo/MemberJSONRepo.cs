84e863801c42d0786426e5604961d2ec27fda0e0
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-04-29 10:01:17 +0200

    udkommenteret memberjsonrepo

--- Lib/Repo/MemberJSONRepo.cs
+++ Lib/Repo/MemberJSONRepo.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections.Generic;
-using System.Text.Json;
-using Lib.Model;
+﻿//using System.Collections.Generic;
+//using System.Text.Json;
+//using Lib.Model;
 
-namespace Lib.Repo
-{
-    public class MemberJSONRepo : MemberCollectionRepo
-    {
-        public MemberJSONRepo()
-        {
-            LoadFile();
-        }
+//namespace TinderForPets.Repo
+//{
+//    public class MemberJSONRepo : MemberCollectionRepo
+//    {
+//        public MemberJSONRepo()
+//        {
+//            LoadFile();
+//        }
 
-        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
-        private void LoadFile()
-        {
-            string path = "member.json";
-            string json = File.ReadAllText(path);
+//        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
+//        private void LoadFile()
+//        {
+//            string path = "member.json";
+//            string json = File.ReadAllText(path);
             
-            _members = JsonSerializer.Deserialize<List<Member>>(json);
-        }
+//            _members = JsonSerializer.Deserialize<List<Member>>(json);
+//        }
 
-        public override void Add(Member member)
-        {
-            base.Add(member);
-            SaveFile();
-        }
+//        public override void Add(Member member)
+//        {
+//            base.Add(member);
+//            SaveFile();
+//        }
 
-        //denne metode skal kaldes når vi vil putte data i vores JSON
-        private void SaveFile()
-        {
-            string path = "members.json";
-            File.WriteAllText(path, JsonSerializer.Serialize(_members));
-        }
-    }
-}
+//        //denne metode skal kaldes når vi vil putte data i vores JSON
+//        private void SaveFile()
+//        {
+//            string path = "members.json";
+//            File.WriteAllText(path, JsonSerializer.Serialize(_members));
+//        }
+//    }
+//}
