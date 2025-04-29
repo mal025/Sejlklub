f4ca0cf325e24be2cc8be088a0d6910984e2bc30
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-04-29 09:53:20 +0200

    JSON save repo

---
+++ Lib/Repo/MemberJSONRepo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Lib.Model;
+
+namespace Lib.Repo
+{
+    public class MemberJSONRepo : MemberCollectionRepo
+    {
+        public MemberJSONRepo()
+        {
+            LoadFile();
+        }
+
+        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
+        private void LoadFile()
+        {
+            string path = "member.json";
+            string json = File.ReadAllText(path);
+            
+            _members = JsonSerializer.Deserialize<List<Member>>(json);
+        }
+
+        public override void Add(Member member)
+        {
+            base.Add(member);
+            SaveFile();
+        }
+
+        //denne metode skal kaldes når vi vil putte data i vores JSON
+        private void SaveFile()
+        {
+            string path = "members.json";
+            File.WriteAllText(path, JsonSerializer.Serialize(_members));
+        }
+    }
+}
