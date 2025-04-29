8d60dfdcb326a71f2abc4d5674419dba21cb1a42
mal025 <mal025@edu.zealand.dk>
2025-04-29 11:00:41 +0200

    Cretead booking repo and service

--- Lib/Repo/BookingCollectionRepo.cs
+++ Lib/Repo/BookingCollectionRepo.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib.Model;
 
 namespace Lib.Repo
 {
-    public class BookingCollectionRepo: IBookingRepo
+    public class BookingCollectionRepo : IBookingRepo
     {
+        public List<Booking> _bookings = new List<Booking>();
+        public void Add(Booking booking)
+        {
+            _bookings.Add(booking);
+        }
+
+        public List<Booking> GetAll()
+        {
+            return _bookings;
+        }
+
+        public void RemoveByID(int id)
+        {
+            _bookings.RemoveAll(booking => booking.ID == id);
+        }
     }
 }
--- Lib/Repo/IBookingRepo.cs
+++ Lib/Repo/IBookingRepo.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib.Model;
 
 namespace Lib.Repo
 {
     public interface IBookingRepo
     {
+        public List<Booking> GetAll();
+        public void Add(Booking booking);
+        public void RemoveByID(int id);
+
+
     }
 }
