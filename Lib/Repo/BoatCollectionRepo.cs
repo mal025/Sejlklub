85fc1b95ea9793bcf88b23662327973e5706efe0
mal025 <mal025@edu.zealand.dk>
2025-04-29 09:45:50 +0200

    Created more repositories

--- Lib/Repo/BoatCollectionRepo.cs
+++ Lib/Repo/BoatCollectionRepo.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib.Model;
 
 namespace Lib.Repo
 {
     public class BoatCollectionRepo : IBoatRepo
     {
+        List<Boat> _boats = new List<Boat>();
 
+        public BoatCollectionRepo()
+        {
+        }
+        public List<Boat> GetAll()
+        {
+            return _boats;
+        }
     }
 }
--- Lib/Repo/IBoatRepo.cs
+++ Lib/Repo/IBoatRepo.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib.Model;
 
 namespace Lib.Repo
 {
     interface IBoatRepo
     {
+        public List<Boat> GetAll();
+
     }
 }
