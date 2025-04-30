18455904f5c3f70118573f1cec306e970287528d
mal025 <mal025@edu.zealand.dk>
2025-04-30 10:42:00 +0200

    Created service for blog

---
+++ Lib/Services/BlogService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.Model;
+using Lib.Repo;
+
+namespace Lib.Services
+{
+    public class BlogService
+    {
+        private IBlogRepo _blogRepo;
+        public BlogService(IBlogRepo blogRepo)
+        {
+            _blogRepo = blogRepo;
+        }
+
+        public void Add(Blog blog)
+        {
+            _blogRepo.Add(blog);
+        }
+
+    }
+}
